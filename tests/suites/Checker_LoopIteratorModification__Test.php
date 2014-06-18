3986c749e0bcf15279053711a6be0a4996aed342
angrybender <dev.temp.null.mail@gmail.com>
2014-06-18 14:05:43 +0400

    fix test

--- tests/suites/Checker_LoopIteratorModification__Test.php
+++ tests/suites/Checker_LoopIteratorModification__Test.php
@@ -2,6 +2,13 @@
 
 include __DIR__ . '/../CheckerSkeleton.php';
 
+\Core\Repository::$function_callback_into_variable = array(
+	'pcntl_wait' => array(1),
+);
+\Core\Repository::$function_callback_into_variable_infinity = array(
+	'sscanf' => 3,
+);
+
 class Checker_VarReAsset extends CheckerSkeleton
 {
 	protected $base_path = 'data/checker_loop_iterator_modification/';
